80a8e23e7cc3414ccec88e7af13d7f2014ab102f
gunse <gunseful@gmail.com>
2024-03-16 12:04:12 +0400

    Revert "test"
    This reverts commit 63fc0f3f2aa89c282228c2439b57559a0f5fa57c.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,9 +4,8 @@ import app.dao.WordsDao;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        WordsDao wordsDao =  new WordsDao();
+        WordsDao wordsDao = new WordsDao();
         wordsDao.getAllWords().forEach(System.out::println);
-
     }
 
 }
