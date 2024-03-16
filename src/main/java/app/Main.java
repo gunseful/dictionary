63fc0f3f2aa89c282228c2439b57559a0f5fa57c
gunse <gunseful@gmail.com>
2024-03-16 12:03:17 +0400

    test

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,8 +4,9 @@ import app.dao.WordsDao;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        WordsDao wordsDao = new WordsDao();
+        WordsDao wordsDao =  new WordsDao();
         wordsDao.getAllWords().forEach(System.out::println);
+
     }
 
 }
